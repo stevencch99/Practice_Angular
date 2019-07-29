75b67be7600186d2af83ef8b94db70e7a601fe55
Steven C <stevencch99@gmail.com>
2019-07-29 23:58:22 +0800

    Add Heroes detail section, shows selected hero\'s name in distinct list item.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+  who: string;
+  onSelect(hero: Hero): void {
+    // ? TODO 這邊的 this 指的是誰
+    this.selectedHero = hero;
+    console.log("TCL: HeroesComponent -> this", this);
+    console.log(typeof(this));
+  }
+
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
     constructor() { }
 
   ngOnInit() {
